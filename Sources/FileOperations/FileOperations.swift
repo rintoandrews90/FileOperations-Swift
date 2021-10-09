b5182dd57ded6e377fae98f0fc5d5a92d1031e77
Rinto Andrews <rintoandrews90@gmail.com>
2021-10-10 03:00:41 +0530

    File Operations Library

--- Sources/FileOperations/FileOperations.swift
+++ Sources/FileOperations/FileOperations.swift
@@ -1,6 +1,18 @@
+
 public struct FileOperations {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    /// DirectoryPath define type of directory in application sandbox
+    ///
+    /// - document: document Directory
+    /// - temp: temp Directory
+    /// - cache: cache Directory
+    public enum DirectoryPath {
+        case document
+        case temp
+        case cache
     }
 }
